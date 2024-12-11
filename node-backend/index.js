6af9ba050f18b10acae6ac007407a9320bb638f4
GreatnessGrace <graceunstoppable7@gmail.com>
2024-12-11 15:43:49 +0530

    dbConnection

--- node-backend/index.js
+++ node-backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { connectDB } = require('./db');
 dotenv.config();  
 
 const app = express();
 const port = process.env.PORT ;
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello from Task Manager App!');
