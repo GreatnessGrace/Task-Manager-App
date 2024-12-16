bc3f12de0c52f2217f3df0ac6e9fce0362c43e66
GreatnessGrace <graceunstoppable7@gmail.com>
2024-12-16 18:06:03 +0530

    signup api integrate

--- Task-Manager-App/src/app/signup/signup.component.ts
+++ Task-Manager-App/src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RestService } from '../services/rest.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private restService: RestService) {}
+
+  onSignup() {
+    const userData = {
+      name: this.name,
+      email: this.email,
+      password: this.password,
+    };
+const url = environment.signup 
+    this.restService.post(url, userData).subscribe(
+      (response) => {
+        console.log('Signup successful', response);
+        // Redirect to login or show success message
+      },
+      (error) => {
+        console.error('Signup error', error);
+        this.errorMessage = 'Failed to create an account. Please try again later.';
+      }
+    );
+  }
 
 }
