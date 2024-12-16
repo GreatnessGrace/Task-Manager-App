ee9f096007ed87c7215a0812828f4c78e62cbb17
GreatnessGrace <graceunstoppable7@gmail.com>
2024-12-16 18:04:28 +0530

    cors

--- node-backend/index.js
+++ node-backend/index.js
@@ -2,15 +2,32 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { connectDB } = require('./db');
 dotenv.config();  
+const userRouter = require("./src/routers/userRoute");
+const cors = require("cors");
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT ;
 
 connectDB();
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  cors({
+    origin: [
+      "http://localhost:4200"
+    ],
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello from Task Manager App!');
 });
+
+app.use("/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Task Manager Server is running on http://localhost:${port}`);
