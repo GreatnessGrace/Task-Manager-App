4d29695e5b6e4a0d900c187e164fa476740b2f2f
GreatnessGrace <graceunstoppable7@gmail.com>
2024-12-31 14:38:07 +0530

    login api in env

--- Task-Manager-App/src/environments/environment.ts
+++ Task-Manager-App/src/environments/environment.ts
@@ -3,5 +3,6 @@ const API_URL = "http://localhost:4000";
 export const environment = {
     production: false,
     
-    signup: API_URL + "/users/signup"
+    signup: API_URL + "/users/signup",
+    login: API_URL + "/users/login"
 }
