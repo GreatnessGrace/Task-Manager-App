ad48c10126cfd02feee405c0314168433ec8cff3
GreatnessGrace <graceunstoppable7@gmail.com>
2024-12-31 16:02:51 +0530

    auth guard implement

--- Task-Manager-App/src/app/app-routing.module.ts
+++ Task-Manager-App/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { AuthGuardService } from './services/auth.guard.service';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'tasks', component: DashboardComponent },
+  {
+    path: 'tasks',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+    canActivate: [AuthGuardService],
+  },
+  { path: '**', redirectTo: 'login' }, // Fallback route
 
 ];
 
