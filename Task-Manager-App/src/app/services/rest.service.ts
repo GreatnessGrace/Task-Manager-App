a1259413a0917ae9f4bbbdfffb449e49f662447e
GreatnessGrace <graceunstoppable7@gmail.com>
2024-12-31 14:12:09 +0530

    rest service debugging

--- Task-Manager-App/src/app/services/rest.service.ts
+++ Task-Manager-App/src/app/services/rest.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  post(url: string, userData: any ): Observable<HttpResponse<any>> {
-    return this.http.post<any>(url, userData, { observe: 'response' });
+  post(url: string, data: any, options = {}): Observable<any> {
+    console.log('Making POST request to:', url); 
+    console.log('Request body:', data); 
+    console.log('Request options:', options); 
+
+    return this.http.post(url, data, options).pipe(
+      catchError(this.handleError) 
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Client-side or network error:', error.error.message);
+    } else {
+      console.error(`Backend returned code ${error.status}, body was:`, error.error);
+    }
+    return throwError('Something went wrong, please try again later.');
   }
 }
