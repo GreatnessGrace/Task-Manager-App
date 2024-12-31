cb873a24ca3ab296f383166829eb3d8ce06bf77d
GreatnessGrace <graceunstoppable7@gmail.com>
2024-12-31 16:01:02 +0530

    auth guard

--- Task-Manager-App/src/app/services/auth.guard.service.ts
+++ Task-Manager-App/src/app/services/auth.guard.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
+  constructor(private router: Router, private authService: AuthService) {}
 
-  constructor() { }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const url: string = state.url;
+    return this.checkLogin(url);
+  }
+
+  private checkLogin(url: string): boolean {
+    const isLoggedIn = this.authService.isAuthenticated();
+    if (!isLoggedIn) {
+      this.router.navigate(['login'], { queryParams: { returnUrl: url } }); // Save return URL for navigation after login
+      return false;
+    }
+    return true;
+  }
 }
