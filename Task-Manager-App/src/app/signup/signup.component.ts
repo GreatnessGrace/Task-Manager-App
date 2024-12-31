e7c0af68dd8128e5f1c597eb3fe5dcf178607c31
GreatnessGrace <graceunstoppable7@gmail.com>
2024-12-31 14:09:58 +0530

    signup api integration

--- Task-Manager-App/src/app/signup/signup.component.ts
+++ Task-Manager-App/src/app/signup/signup.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RestService } from '../services/rest.service';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
   name: string = '';
   email: string = '';
   password: string = '';
   errorMessage: string = '';
 
-  constructor(private restService: RestService) {}
+  constructor(private restService: RestService, private router: Router, private notificationService: NotificationService
+  ) {}
+
+  ngOnInit(): void {
+   
+  }
 
   onSignup() {
     const userData = {
@@ -21,17 +28,43 @@ export class SignupComponent {
       email: this.email,
       password: this.password,
     };
-const url = environment.signup 
-    this.restService.post(url, userData).subscribe(
+    const url = environment.signup;
+  
+    this.restService.post(url, userData, { observe: 'response' }).subscribe(
       (response) => {
-        console.log('Signup successful', response);
-        // Redirect to login or show success message
+        console.log('Response from RestService:', response);
+        const responseBody = response.body;
+        console.log('Response body:', responseBody);
+  
+        if (response.status === 201) {
+          this.notificationService.showSuccess('Signup successful!');
+          this.router.navigate(['/login']);
+        } else if (responseBody && responseBody.message) {
+          console.log("here")
+          if (responseBody.message == "User already exists") {
+            console.log("there")
+
+            this.notificationService.showError('User already exists. Please try with a different email.');
+          } else {
+            console.log("hre")
+
+            this.notificationService.showError('Signup failed. Please try again.');
+          }
+        } else {
+          console.log("he")
+
+          this.notificationService.showError('Signup failed. Please try again.');
+        }
       },
       (error) => {
-        console.error('Signup error', error);
-        this.errorMessage = 'Failed to create an account. Please try again later.';
+        console.error('Signup error:', error);
+        this.notificationService.showError('Signup failed. Please try again.');
       }
     );
   }
+  
+  
+  
+  
 
 }
